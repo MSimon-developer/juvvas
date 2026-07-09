9e53ec7791dec9556ee994f1fd7c65df9c256d56
MSimon-developer <simonmainamugo@gmail.com>
2026-07-09 10:21:48 +0300

    Add dark mode and improve metadata

--- juvvas-next/app/layout.tsx
+++ juvvas-next/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import WhatsAppButton from "@/components/layout/WhatsAppButton";
+import { ThemeProvider } from "@/components/layout/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,8 +19,71 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Juvvas",
-  description: "Find rentals, Airbnbs and properties for sale in Kenya",
+  metadataBase: new URL("https://juvvas.com"),
+
+  title: {
+    default: "Juvvas",
+    template: "%s | Juvvas",
+  },
+
+  description:
+    "Find rentals, Airbnbs and properties for sale in Kenya.",
+
+  keywords: [
+    "Kenya real estate",
+    "Property for sale",
+    "Rentals",
+    "Airbnb",
+    "Homes",
+    "Apartments",
+    "Land",
+    "Real Estate Kenya",
+    "Juvvas",
+  ],
+
+  icons: {
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      {
+        url: "/FAVICONJUVVAS2.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/favicon.ico",
+    apple: "/FAVICONJUVVAS2.png",
+  },
+
+  openGraph: {
+    title: "Juvvas",
+    description:
+      "Find rentals, Airbnbs and properties for sale in Kenya.",
+    url: "https://juvvas.com",
+    siteName: "Juvvas",
+    images: [
+      {
+        url: "/FAVICONJUVVAS2.png",
+        width: 512,
+        height: 512,
+        alt: "Juvvas",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Juvvas",
+    description:
+      "Find rentals, Airbnbs and properties for sale in Kenya.",
+    images: ["/FAVICONJUVVAS2.png"],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
@@ -30,18 +94,25 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col">
-        <Navbar />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
 
-        <main className="flex-1">
-          {children}
-        </main>
+          <main className="flex-1">
+            {children}
+          </main>
 
-        <Footer />
+          <Footer />
           <WhatsAppButton />
-
+        </ThemeProvider>
       </body>
     </html>
   );
